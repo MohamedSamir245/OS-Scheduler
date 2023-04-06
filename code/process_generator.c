050bbcf28bbd5be8090cf16530fc84c35abc2e91
Mohamed Samir <mohamedsamir2452002@gmail.com>
2023-04-06 23:58:46 +0200

    Process Data Structure
    not sure

--- code/process_generator.c
+++ code/process_generator.c
@@ -2,8 +2,94 @@
 
 void clearResources(int);
 
-int main(int argc, char * argv[])
+struct Process
 {
+    int id;
+    int executionTime;
+    int priority;
+    int arrivalTime;
+    int finishTime;
+    int remainingTime;
+};
+
+void Process__init(struct Process *self, int id, int ar, int run, int p)
+{
+    self->arrivalTime = ar;
+    self->priority = p;
+    self->executionTime = run;
+    self->id = id;
+    self->remainingTime = run;
+}
+
+struct Process *Process__create(int id, int ar, int run, int p)
+{
+    struct Process *result = (struct Process *)malloc(sizeof(struct Process));
+    Process__init(result, id, ar, run, p);
+    return result;
+}
+
+void Process__reset(struct Process *self)
+{
+}
+
+void Process__destroy(struct Process *process)
+{
+    if (process)
+    {
+        Process__reset(process);
+        free(process);
+    }
+}
+
+int Process__excutionTime(struct Process *self)
+{
+    return self->executionTime;
+}
+
+int Process__arrivalTime(struct Process *self)
+{
+    return self->arrivalTime;
+}
+
+int Process__remainingTime(struct Process *self)
+{
+    return self->remainingTime;
+}
+
+int Process__finishTime(struct Process *self)
+{
+    return self->finishTime;
+}
+
+int Process__priority(struct Process *self)
+{
+    return self->priority;
+}
+
+int Process__id(struct Process *self)
+{
+    return self->id;
+}
+
+int main(int argc, char *argv[])
+{
+    // Samir Area
+
+    struct Process *processes[5];
+
+    processes[0] = Process__create(0, 0, 5, 2);
+    processes[1] = Process__create(1, 1, 6, 4);
+    processes[2] = Process__create(2, 2, 7, 3);
+    processes[3] = Process__create(3, 3, 8, 2);
+    processes[4] = Process__create(4, 4, 9, 5);
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("PID = %d, ExTime = %d, Arrival Time = %d, Priority = %d, Remaining Time = %d, Finish Time = %d\n", processes[i]->id, processes[i]->executionTime, processes[i]->arrivalTime, processes[i]->priority, processes[i]->remainingTime, processes[i]->finishTime);
+    }
+
+    //
+
     signal(SIGINT, clearResources);
     // TODO Initialization
     // 1. Read the input files.
@@ -23,5 +109,5 @@ int main(int argc, char * argv[])
 
 void clearResources(int signum)
 {
-    //TODO Clears all resources in case of interruption
+    // TODO Clears all resources in case of interruption
 }

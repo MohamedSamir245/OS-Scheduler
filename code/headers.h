133b2e39ecb85c9f2f8f0035fbf0b7a53962ea0b
Mohamed Taher <muhammed.snd22@gmail.com>
2023-04-08 17:09:17 +0200

    Queue DS to header

--- code/headers.h
+++ code/headers.h
@@ -96,6 +96,91 @@ int Process__id(struct Process *self)
 // ======================================
 
 
+
+// ======================================
+// =============  Queue =================
+// ======================================
+
+// Node 
+typedef struct node { 
+    struct Process* data; 
+  
+    // Lower values indicate higher priority 
+    int priority; 
+  
+    struct node* next; 
+  
+} Node; 
+  
+// Function to Create A New Node 
+Node* newNode(struct Process* p, int x) 
+{ 
+    Node* temp = (Node*)malloc(sizeof(Node)); 
+    temp->data = p; 
+    temp->priority = x; 
+    temp->next = NULL; 
+  
+    return temp; 
+} 
+  
+// Return the value at head 
+struct Process* peek(Node** head) 
+{ 
+    return (*head)->data; 
+} 
+  
+// Removes the element with the 
+// highest priority from the list 
+void pop(Node** head) 
+{ 
+    Node* temp = *head; 
+    (*head) = (*head)->next; 
+    free(temp); 
+} 
+  
+// Function to push according to priority 
+void push(Node** head, struct Process* P, int x) 
+{ 
+    Node* start = (*head); 
+  
+    // Create new Node 
+    Node* temp = newNode(P, x); 
+  
+    // Special Case: The head of list has lesser 
+    // priority than new node. So insert new 
+    // node before head node and change head node. 
+    if ((*head)->priority > x) { 
+  
+        // Insert New Node before head 
+        temp->next = *head; 
+        (*head) = temp; 
+    } 
+    else { 
+  
+        // Traverse the list and find a 
+        // position to insert new node 
+        while (start->next != NULL && 
+            start->next->priority < x) { 
+            start = start->next; 
+        } 
+  
+        // Either at the ends of the list 
+        // or at required position 
+        temp->next = start->next; 
+        start->next = temp; 
+    } 
+} 
+  
+// Function to check is list is empty 
+int isEmpty(Node** head) 
+{ 
+    return (*head) == NULL; 
+} 
+// ======================================
+// ======================================
+// ======================================
+
+
 ///==============================
 //don't mess with this variable//
 int * shmaddr;                 //

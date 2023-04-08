332e246a5b088bb1b1c0cc40fc99b7b9396ac463
Mohamed Sanad <96536944+Mohamed-Sanad@users.noreply.github.com>
2023-04-08 17:42:22 +0200

    add addProcess fn

--- code/scheduler.c
+++ code/scheduler.c
@@ -1,9 +1,32 @@
 #include "headers.h"
 
-
-
 int algo;
 
+void addProcess (struct Node* head,struct Process* pNew)
+{
+    if(algo==1){
+        if(!head)
+        {
+            head=newNode(pNew,pNew->priority);
+        }
+        push(& head,pNew,pNew->priority);
+    }
+    else if(algo==2){
+        push(& head,pNew,pNew->remainingTime);
+        if(!head)
+        {
+            head=newNode(pNew,pNew->remainingTime);
+        }
+    }
+    else{
+        push(& head,pNew,0);
+        if(!head)
+        {
+            head=newNode(pNew,0);
+        }
+    }
+    
+}
 
 int main(int argc, char * argv[])
 {
@@ -11,6 +34,9 @@ int main(int argc, char * argv[])
     
     //TODO implement the scheduler :)
     //upon termination release the clock resources.
+
+    struct Node* qHead=NULL; 
+    
     
     destroyClk(true);
 }

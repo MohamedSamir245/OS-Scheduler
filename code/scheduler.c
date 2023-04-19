4278e9bad8403dfb0b1a888448913014aa70dff7
Mohamed Sanad <96536944+Mohamed-Sanad@users.noreply.github.com>
2023-04-20 00:01:05 +0200

    Eid's cleaning (:

--- code/scheduler.c
+++ code/scheduler.c
@@ -1,10 +1,175 @@
+// ===============================================================================================
+// =====================================    Libraries   ==========================================
+// ===============================================================================================
+#pragma once
 #include "headers.h"
+#include "math.h"
+
+// ===============================================================================================
+// =====================================        Functions       ==================================
+// ===============================================================================================
+struct Process *reader(int shmid);
 void addProcess(struct Process *);
+void printSchedulerPerf();
+void Central_Processing_Unit();
 void switch_algo_ossama();
+void switch_HPF();
+void switch_SRTN();
+void switch_RR();
+
+// ===============================================================================================
+// =====================================    Global Variables    ==================================
+// ===============================================================================================
 int algo = 1, currentclk; // TODO: receive algorithim value
+
+int utility_time = 0;
+int processes_num = 0;
+int sum_times = 0;
+int sum_waiting = 0;
+
+int TA_arr[5000] = {0};
+int kk = 0;
+
 struct Node *qHead = NULL;
 int id, arrTime, exTime, priority, finishTime, remainingTime;
 struct Process *running_process_ossama = NULL;
+
+// For Round-Robin
+int clock = 0;
+int quanta = 0;
+int RR_clock = 0;
+
+// ===============================================================================================
+// =====================================    Code   ===============================================
+// ===============================================================================================
+
+int main(int argc, char *argv[])
+{
+    initClk();
+
+    // TODO implement the scheduler :)
+    // upon termination release the clock resources.
+
+    int schedulerShmId = shmget(250, 128, IPC_CREAT | 0644);
+    int mesq_id = msgget(250, 0666 | IPC_CREAT);
+
+    if ((long)schedulerShmId == -1)
+    {
+        perror("Error in creating shm!");
+        exit(-1);
+    }
+    if (mesq_id == -1)
+    {
+        perror("Error in creating MesQ!");
+        exit(-1);
+    }
+
+    printf("\nScheduler Shared memory ID = %d\n", schedulerShmId);
+    printf("Mesq_ID = %d\n", mesq_id);
+    // printf("Shared memory ID = %d\n", shmid);
+
+    struct Process *shmaddr = (struct Process *)shmat(schedulerShmId, (void *)0, 0);
+    struct msgbuff message;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    struct Process *ttt;
+    int prevclk = getClk();
+    // printf("\nIam here\n");
+
+    struct msqid_ds __buf;
+
+    msgctl(mesq_id, IPC_STAT, &__buf);
+    int prevqnum = __buf.msg_qnum;
+    int rec_val;
+    while (1)
+    {
+        if (prevclk != getClk())
+        {
+            //
+            msgctl(mesq_id, IPC_STAT, &__buf);
+
+            // printf("\nBefore\n%d\n", __buf.msg_qnum);
+
+            int currqnum = __buf.msg_qnum;
+            while (currqnum != prevqnum)
+            {
+                rec_val = msgrcv(mesq_id, &message, sizeof(message.request), 0, !IPC_NOWAIT);
+                // __buf.msg_qnum--;
+                // msgctl(mesq_id, IPC_SET, &__buf);
+                // msgctl(mesq_id, IPC_RMID, NULL);
+
+                // printf("\nAfter\n%d\n", __buf.msg_qnum);
+
+                if (message.request == 1)
+                {
+
+                    printf("Ismail Enters read\n");
+                    ttt = reader(schedulerShmId); // TODO: need modification to read all added processes
+                                                  // it reads only one
+                    // int send_val = msgsnd(mesq_id, &message, sizeof(message.request), !IPC_NOWAIT);
+
+                    printf("Ismail exit read\n");
+                }
+
+                prevqnum = currqnum;
+            }
+
+            if (ttt && ttt->executionTime != 0)
+            {
+                // printf("Delete: i am in if\n");
+                // do something
+                addProcess(ttt);
+                if (ttt->id > processes_num)
+                    processes_num = ttt->id;
+                // use data;
+                // printf("Delete: i am going out if\n");
+            }
+            printf("Prevclk = %d | current clk = %d\n", prevclk, getClk());
+            currentclk = getClk();
+            Central_Processing_Unit();
+            prevclk = getClk();
+
+            // sleep(2);
+            utility_time++;
+        }
+    }
+
+    printSchedulerPerf();
+
+    printf("%d\n\n\n\n", schedulerShmId);
+    shmctl(schedulerShmId, IPC_RMID, (struct shmid_ds *)0);
+    printf("After%d\n\n\n\n", schedulerShmId);
+
+    // }
+
+    destroyClk(true);
+}
+
+// ===============================================================================================
+// =====================================    Functions Implementation    ==========================
+// ===============================================================================================
+
+void printSchedulerPerf()
+{
+    printf("CPU utilization = %d%%\n", 100.0 * utility_time / getClk());
+    double avg_wTA = 1.0 * sum_times / processes_num;
+    printf("Avg WTA = %d\n", avg_wTA);
+    printf("Avg Waiting = %d\n", 1.0 * sum_waiting / processes_num);
+    double square_sum = 0;
+    for (int i = 0; i < kk; i++)
+    {
+        square_sum += (TA_arr[i] - avg_wTA) * (TA_arr[i] - avg_wTA);
+    }
+    double std = sqrt(square_sum / processes_num);
+    printf("Std WTA = %d\n", std);
+}
+
+// #At time x process y state arr w total z remain y wait k
+void printSchedulerLog(int time, int pId, char *state, int arr, int total, int remain, int wait)
+{
+    printf("At time %d process %d %s arr %d total %d remain %d wait %d", time, pId, state, arr, total, remain, wait);
+}
 
 // reader: read processes from "Shared Memory"
 struct Process *reader(int shmid)
@@ -109,7 +274,16 @@ void Central_Processing_Unit()
     {
         running_process_ossama->remainingTime--;
         if (!running_process_ossama->remainingTime)
+        {
+            running_process_ossama->turnaroundTime = currentclk - running_process_ossama->arrivalTime;
+            running_process_ossama->weightedTATime = running_process_ossama->turnaroundTime / running_process_ossama->executionTime;
+            TA_arr[kk] = running_process_ossama->weightedTATime;
+            kk++;
+            sum_times += running_process_ossama->weightedTATime;
+            running_process_ossama->waitingTime = running_process_ossama->startTime - running_process_ossama->arrivalTime;
+            sum_waiting += running_process_ossama->waitingTime;
             running_process_ossama = NULL;
+        }
     }
 }
 
@@ -129,6 +303,10 @@ void switch_HPF()
         if (running_process_ossama)
         {
             pop(&qHead);
+            if (running_process_ossama->startTime == -1)
+            {
+                running_process_ossama->startTime = currentclk;
+            }
         }
         printf("Delete: HPF - out pop\n");
     }
@@ -146,8 +324,12 @@ void switch_SRTN()
         printf("Delete: SRTN - out peek\n");
         printf("Delete: SRTN - in pop\n");
         pop(&qHead);
+        if (running_process_ossama->startTime == -1)
+        {
+            running_process_ossama->startTime = currentclk;
+        }
         printf("Delete: SRTN - out pop\n");
-                printf("Running process address is %x\n  ", running_process_ossama);
+        printf("Running process address is %x\n  ", running_process_ossama);
         printf("remaining time = %d\n", running_process_ossama->remainingTime);
     }
     else if (running_process_ossama && peek(&qHead))
@@ -156,14 +338,15 @@ void switch_SRTN()
         {
             addProcess(running_process_ossama);
             running_process_ossama = peek(&qHead);
-            pop(qHead);
+            pop(&qHead);
+            if (running_process_ossama->startTime == -1)
+            {
+                running_process_ossama->startTime = currentclk;
+            }
         }
     }
 }
 
-int clock = 0;
-int quanta = 0;
-int RR_clock = 0;
 // RR Algorithim
 void switch_RR()
 {
@@ -171,6 +354,10 @@ void switch_RR()
     {
         running_process_ossama = peek(&qHead);
         pop(&qHead);
+        if (running_process_ossama->startTime == -1)
+        {
+            running_process_ossama->startTime = currentclk;
+        }
         RR_clock = currentclk;
     }
     else if (currentclk - RR_clock == quanta)
@@ -180,6 +367,10 @@ void switch_RR()
         if (running_process_ossama)
         {
             pop(&qHead);
+            if (running_process_ossama->startTime == -1)
+            {
+                running_process_ossama->startTime = currentclk;
+            }
             RR_clock = currentclk;
         }
     }
@@ -197,105 +388,4 @@ void switch_algo_ossama()
     }
     else
         switch_RR();
-}
-
-int main(int argc, char *argv[])
-{
-    initClk();
-
-    // TODO implement the scheduler :)
-    // upon termination release the clock resources.
-
-    int schedulerShmId = shmget(250, 128, IPC_CREAT | 0644);
-    int mesq_id = msgget(250, 0666 | IPC_CREAT);
-
-    if ((long)schedulerShmId == -1)
-    {
-        perror("Error in creating shm!");
-        exit(-1);
-    }
-    if (mesq_id == -1)
-    {
-        perror("Error in creating MesQ!");
-        exit(-1);
-    }
-
-    printf("\nScheduler Shared memory ID = %d\n", schedulerShmId);
-    printf("Mesq_ID = %d\n", mesq_id);
-    // printf("Shared memory ID = %d\n", shmid);
-
-    struct Process *shmaddr = (struct Process *)shmat(schedulerShmId, (void *)0, 0);
-    struct msgbuff message;
-
-    // for (int i = 0; i < 5; i++)
-    // {
-    struct Process *ttt;
-    int prevclk = getClk();
-    // printf("\nIam here\n");
-
-    struct msqid_ds __buf;
-
-    msgctl(mesq_id, IPC_STAT, &__buf);
-    int prevqnum = __buf.msg_qnum;
-    int rec_val;
-    while (1)
-    {
-        if (prevclk != getClk())
-        {
-            //
-            msgctl(mesq_id, IPC_STAT, &__buf);
-
-            // printf("\nBefore\n%d\n", __buf.msg_qnum);
-
-            int currqnum = __buf.msg_qnum;
-            while (currqnum != prevqnum)
-            {
-                rec_val = msgrcv(mesq_id, &message, sizeof(message.request), 0, !IPC_NOWAIT);
-                // __buf.msg_qnum--;
-                // msgctl(mesq_id, IPC_SET, &__buf);
-                // msgctl(mesq_id, IPC_RMID, NULL);
-
-                // printf("\nAfter\n%d\n", __buf.msg_qnum);
-
-                if (message.request == 1)
-                {
-
-                    printf("Ismail Enters read\n");
-                    ttt = reader(schedulerShmId); // TODO: need modification to read all added processes
-                                                  // it reads only one
-                    // int send_val = msgsnd(mesq_id, &message, sizeof(message.request), !IPC_NOWAIT);
-
-                    printf("Ismail exit read\n");
-                }
-
-                prevqnum = currqnum;
-            }
-
-            if (ttt && ttt->executionTime != 0)
-            {
-                // printf("Delete: i am in if\n");
-                // do something
-                addProcess(ttt);
-                // use data;
-                // printf("Delete: i am going out if\n");
-            }
-            printf("Prevclk = %d | current clk = %d\n", prevclk, getClk());
-            currentclk = getClk();
-            Central_Processing_Unit();
-            prevclk = getClk();
-
-            // sleep(2);
-        }
-    }
-    //    Central_Processing_Unit();
-    //    Central_Processing_Unit();
-    //    Central_Processing_Unit();
-
-    printf("%d\n\n\n\n", schedulerShmId);
-    shmctl(schedulerShmId, IPC_RMID, (struct shmid_ds *)0);
-    printf("After%d\n\n\n\n", schedulerShmId);
-
-    // }
-
-    destroyClk(true);
 }

788d27a5e6921170da94398919f498aac73bfe60
Mohamed Sanad <96536944+Mohamed-Sanad@users.noreply.github.com>
2023-04-29 17:40:13 +0200

    ماية المخلل

--- code/mem.c
+++ code/mem.c
@@ -1,32 +1,33 @@
 
 #include "headers.h"
-static struct treeNode *Root=NULL;
-Root=createTreeNode(0,1024,0,(struct treeNode*)NULL);
 
+struct treeNode *Root = NULL;
 
+void main()
+{
+    Root = createTreeNode(0, 1024, 0, (struct treeNode *)NULL);
 
+    // int currSize=0;
+    // tn={-1,nStart,nSize,nPosition,NULL,NULL,NULL};
+    //,tempNode;
+    // tempNode={-1,0,0,0};
+    // struct treeNode* arr[1024];
+    // arr[0]=Root;
+    // currSize+=1;
+    //  currSize = &tmp;
+    // tmp = 1;
 
+    // printf("%d",currSize);
 
-//int currSize=0;
-//tn={-1,nStart,nSize,nPosition,NULL,NULL,NULL};
-//,tempNode;
-//tempNode={-1,0,0,0};
-//struct treeNode* arr[1024];
-//arr[0]=Root;
-//currSize+=1;
-// currSize = &tmp;
-//tmp = 1;
+    // array[0] = createTreeNode(0, 1024, 0, NULL);
 
-//printf("%d",currSize);
+    int psize1 = 300;
+    int psize2 = 200;
+    int psize3 = 256;
+    int psize4 = 512;
 
-// array[0] = createTreeNode(0, 1024, 0, NULL);
-
-int psize1 = 300;
-int psize2 = 200;
-int psize3 = 256;
-int psize4 = 512;
-
-//printf("Allocated = %d, Now size is %d", allocateMemory(arr, currSize, psize1, 1), currSize);
-// printf("Allocated = %d, Now size is %d", allocateMemory(array, currSize, psize2, 2), currSize);
-// printf("Allocated = %d, Now size is %d", allocateMemory(array, currSize, psize3, 3), currSize);
-// printf("Allocated = %d, Now size is %d", allocateMemory(array, currSize, psize4, 4), currSize);
+    // printf("Allocated = %d, Now size is %d", allocateMemory(arr, currSize, psize1, 1), currSize);
+    //  printf("Allocated = %d, Now size is %d", allocateMemory(array, currSize, psize2, 2), currSize);
+    //  printf("Allocated = %d, Now size is %d", allocateMemory(array, currSize, psize3, 3), currSize);
+    //  printf("Allocated = %d, Now size is %d", allocateMemory(array, currSize, psize4, 4), currSize);
+}

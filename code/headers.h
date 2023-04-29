5bb43a80c01ba92cdf80b9b7185f25048f302dd9
Mohamed Sanad <96536944+Mohamed-Sanad@users.noreply.github.com>
2023-04-29 23:01:03 +0200

    Added squareRoot Fn.

--- code/headers.h
+++ code/headers.h
@@ -29,6 +29,32 @@ struct msgbuff
 // ============================
 //
 
+double squareRoot(double n)
+{
+    // Max and min are used to take into account numbers less than 1
+    double lo = (1 < n) ? 1 : n;
+    double hi = (1 > n) ? 1 : n;
+    double mid;
+
+    // Update the bounds to be off the target by a factor of 10
+    while (100 * lo * lo < n)
+        lo *= 10;
+    while (0.01 * hi * hi > n)
+        hi *= 0.1;
+
+    for (int i = 0; i < 100; i++)
+    {
+        mid = (lo + hi) / 2;
+        if (mid * mid == n)
+            return mid;
+        if (mid * mid > n)
+            hi = mid;
+        else
+            lo = mid;
+    }
+    return mid;
+}
+
 // ======================================
 // ===========  Process =================
 // ======================================

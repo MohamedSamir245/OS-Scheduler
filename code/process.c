c48849d24279312ab14c210116ae40b89eb9aa38
Mohamed Sanad <96536944+Mohamed-Sanad@users.noreply.github.com>
2023-04-29 21:46:14 +0200

    Commented Printing.

--- code/process.c
+++ code/process.c
@@ -2,7 +2,7 @@
 
 /* Modify this file as needed*/
 
-int prevclk=-1;
+int prevclk = -1;
 bool start_process()
 {
     int i = fork();
@@ -18,31 +18,30 @@ int main(int agrc, char *argv[])
     shmid = shmget(key_id, 4096, IPC_CREAT | 0644);
     if (shmid == -1)
     {
-        perror("Error in create");
+        perror("Process.c: Error in create");
         exit(-1);
     }
-    else
-        printf("\nShared memory ID = %d\n", shmid);
+    // else
+    // printf("\nShared memory ID = %d\n", shmid);
 
-
-    int *shmaddr = (int*)shmat(shmid, (void *)0, 0);
+    int *shmaddr = (int *)shmat(shmid, (void *)0, 0);
     if (shmaddr == -1)
     {
-        perror("Error in attach in reader");
+        perror("Process.c: Error in attach in reader");
         exit(-1);
     }
-    printf("\nReader: Shared memory attached at address %x\n", shmaddr);
+    // printf("\nReader: Shared memory attached at address %x\n", shmaddr);
 
     // TODO it needs to get the remaining time from somewhere
     // int *remainingtime = (int*)shmaddr;
     while (*shmaddr > 0)
     {
-        if(prevclk!=getClk()){
+        if (prevclk != getClk())
+        {
             (*shmaddr)--;
-            prevclk=getClk();
+            prevclk = getClk();
         }
     }
-
 
     shmdt(shmaddr);
     destroyClk(false);
